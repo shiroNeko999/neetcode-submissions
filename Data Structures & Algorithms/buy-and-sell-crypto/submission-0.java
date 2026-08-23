cd1605722fd1f7bd469de386dc2f198350ad530a
shiroNeko999 <158245875+shiroNeko999@users.noreply.github.com>
2026-08-24 01:01:48 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+
+        int l = 0; int r = 0;
+        int profit = 0;
+        while( r< prices.length){
+           if(prices[l]<=prices[r]){
+            profit = Math.max(profit, (prices[r]-prices[l]));
+            r++;
+           } else{
+            l= r;
+           }
+        }
+        return profit;
+    }
+}
